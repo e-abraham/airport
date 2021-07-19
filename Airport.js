25e3569575b01d62b9fb80b818f30c04160fdd6f
elizabeth <elizabethannabraham@outlook.com>
2021-07-19 00:20:26 -0500

    Create async methods and tests

--- Airport.js
+++ Airport.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const fsp = require("fs").promises;
+
 class Airport {
     constructor(name){
         this.name = name;
@@ -8,6 +12,66 @@ class Airport {
         this.planes.push(plane);
 
     }
+
+    getInfoCallback(callback){
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, "airportsData.json");
+        fs.readFile(dbPath, (err, data) => {
+            const airports = JSON.parse(data)
+            const airport = airports.find(airport => airport.iata === airportName)
+            callback(err, airport)
+        })
+    }
+
+    getInfoPromise(){
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, "airportsData.json");
+
+        return new Promise((resolve, reject) => {
+            fs.readFile(dbPath, (err, data) => {
+                if(err) {
+                    return reject(err)
+                }
+
+                const airports = JSON.parse(data)
+                const airport = airports.find(airport => airport.iata === airportName)
+                
+                resolve(airport)
+            })
+        })
+    }
+
+    async getInfoAwait(){
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, "airportsData.json");
+
+        try {
+            const data = await fsp.readFile(dbPath);
+            const allAirports = JSON.parse(data);
+            const airport = allAirports.find(airport => airport.iata === airportName);
+            return airport;
+        } catch(err) {
+            console.log(err);
+        }
+
+    }
+
+    async getSameCity(){
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, "airportsData.json");
+
+        try {
+            const data = await fsp.readFile(dbPath);
+            const allAirports = JSON.parse(data);
+            const airport = allAirports.find(airport => airport.iata === airportName);
+            const airportCity = airport.city;
+            const list = allAirports.filter(airport => airport.city === airportCity && airport.name != airportName)
+            return list;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
 }
 
 module.exports = Airport;
--- Airport.test.js
+++ Airport.test.js
@@ -1,26 +1,65 @@
 const Airport = require("./Airport");
 const Plane = require("./Plane");
 
+
 describe("Airport object", () => {
-    const testAirport = new Airport("DFW");
+    const DFW = new Airport("DFW");
 
     test("airport has name", () => {
-        expect(testAirport.name).toBe("DFW");
+        expect(DFW.name).toBe("DFW");
     })
 
     test("airport has planes array", () => {
-        expect(Array.isArray(testAirport.planes)).toBe(true);
+        expect(Array.isArray(DFW.planes)).toBe(true);
     })
 
     test("airport can add plane", () => {
         const testPlane = new Plane("B1R2");
-        testAirport.addPlane(testPlane);
-        expect(testAirport.planes.length).toBe(1);
+        DFW.addPlane(testPlane);
+        expect(DFW.planes.length).toBe(1);
     })
 
-    // test("can access weight of specific bag", () => {
-    //     testWeight = testAirport.bagWeight(testPlane, testPassenger, testBag);
-    // })
+
+    test("callback: airports have a country", (done) => {
+        const CDG = new Airport("CDG");
+        //when
+        CDG.getInfoCallback((err,info) => {
+            //then
+            console.log(info);
+            expect(err).toBeNull();
+            expect(info.country).toEqual("FR");
+            done()
+        })
+    })
+
+
+    test("promise: airports have a city", () => {
+        // const DFW = new Airport("DFW")
+        //when
+        return DFW.getInfoPromise()
+        .then((data) => {
+            //then
+            expect(data.city).toEqual("Dallas-Fort Worth");
+        })
+        .catch((err) => {
+            expect(err).toBeNull();
+        })
+    })
+
+    test("async/await: airports have a state", async () => {
+        // const DFW = new Airport("DFW")
+        const airport = await DFW.getInfoAwait()
+        console.log(airport);
+        expect(airport.state).toEqual("Texas");
+
+    })
+
+    test("async/await: list of airports in same city", async () => {
+        const DAL = new Airport("DAL")
+        const list = await DAL.getSameCity()
+        console.log(list);
+        expect(list.length).toBe(8);
+    })
 
 
 })
